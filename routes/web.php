f95350f4e39ec359a412be44945bb6da67e7a523
Thomas Jungers <thomas.jungers@minfin.fed.be>
2024-09-20 17:38:45 +0000

    Require auth for routes

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -17,6 +13,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
+    Route::get('/', function () {
+        return view('welcome');
+    });
     Route::get('/m/{member:qr_key}', [MemberController::class, 'show_card']);
     Route::resource('members', MemberController::class);
 });

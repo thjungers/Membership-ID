6e883d650ff841bf04e35b64af203edd583696cd
Thomas Jungers <thomas.jungers@minfin.fed.be>
2024-09-20 17:36:43 +0000

    Install Laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/m/{member:qr_key}', [MemberController::class, 'show_card']);
-Route::resource('members', MemberController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/m/{member:qr_key}', [MemberController::class, 'show_card']);
+    Route::resource('members', MemberController::class);
+});
+
+require __DIR__.'/auth.php';

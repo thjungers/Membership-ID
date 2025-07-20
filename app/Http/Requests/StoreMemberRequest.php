24988393296d3cbee83e3667534d7a8e1326614d
Thomas Jungers <thomas.jungers@euranova.eu>
2025-07-20 18:58:27 +0000

    Set authorization rules

--- app/Http/Requests/StoreMemberRequest.php
+++ app/Http/Requests/StoreMemberRequest.php
@@ -11,7 +11,7 @@ class StoreMemberRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check();
     }
 
     /**
--- app/Http/Requests/UpdateMemberRequest.php
+++ app/Http/Requests/UpdateMemberRequest.php
@@ -11,7 +11,7 @@ class UpdateMemberRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return true;
+        return auth()->check();
     }
 
     /**

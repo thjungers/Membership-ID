4c943d7e19498d29efbb463ae7bd9a0f0c9004e4
Thomas Jungers <thomas.jungers@euranova.eu>
2025-11-11 14:16:48 +0000

    Add fr localization

---
+++ lang/fr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'Votre mot de passe a été réinitialisé.',
+    'sent' => 'Le lien de réinitialisation du mot de passe a été envoyé par e-mail.',
+    'throttled' => 'Attendez avant de réessayer.',
+    'token' => 'Le jeton de réinitialisation du mot de passe est invalide.',
+    'user' => "Il n'existe pas d'utilisateur avec cette adresse e-mail.",
+
+];

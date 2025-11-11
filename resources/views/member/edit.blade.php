3bfe6fe47bbfb1adb0c78b8caccb71df0da9ccee
Thomas Jungers <thomas.jungers@euranova.eu>
2025-11-11 15:03:08 +0000

    Fix read of pin on create

--- resources/views/member/edit.blade.php
+++ resources/views/member/edit.blade.php
@@ -13,35 +13,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form
-                        action="/members/{{ $member?->id }}"
-                        method="POST"
-                        class="space-y-6"
-                    >
+                    <form action="/members/{{ $member?->id }}" method="POST" class="space-y-6">
                         @csrf
                         @if ($member)
-                            @method("PATCH")                    
+                            @method('PATCH')
                         @endif
-                        
+
                         <div>
                             <x-input-label for="update_first_name" value="Prénom" />
-                            <x-text-input id="update_first_name" name="first_name" required class="mt-1 block w-full" :value="old('first_name', $member?->first_name)" />
+                            <x-text-input id="update_first_name" name="first_name" required class="mt-1 block w-full"
+                                :value="old('first_name', $member?->first_name)" />
                             <x-input-error class="mt-2" :messages="$errors->get('first_name')" />
                         </div>
                         <div>
                             <x-input-label for="update_last_name" value="Nom de famille" />
-                            <x-text-input id="update_last_name" name="last_name" required class="mt-1 block w-full" style="font-variant: small-caps" :value="old('last_name', $member?->last_name)" />
+                            <x-text-input id="update_last_name" name="last_name" required class="mt-1 block w-full"
+                                style="font-variant: small-caps" :value="old('last_name', $member?->last_name)" />
                             <x-input-error class="mt-2" :messages="$errors->get('last_name')" />
                         </div>
                         <div>
                             <x-input-label for="update_email" value="Adresse e-mail" />
-                            <x-text-input id="update_email" name="email" type="email" required class="mt-1 block w-full" :value="old('email', $member?->email)" />
+                            <x-text-input id="update_email" name="email" type="email" required
+                                class="mt-1 block w-full" :value="old('email', $member?->email)" />
                             <x-input-error class="mt-2" :messages="$errors->get('email')" />
                         </div>
-                        <div>
-                            <x-input-label for="pin" value="PIN" />
-                            <x-text-input id="pin" name="pin" type="text" disabled class="mt-1 block w-full" :value="$member->pin" />
-                        </div>
+                        @if ($member !== null)
+                            <div>
+                                <x-input-label for="pin" value="PIN" />
+                                <x-text-input id="pin" name="pin" type="text" disabled
+                                    class="mt-1 block w-full" :value="$member->pin" />
+                            </div>
+                        @endif
                         <div>
                             <x-primary-button>{{ $member ? 'Modifier' : 'Créer' }}</x-primary-button>
                         </div>
